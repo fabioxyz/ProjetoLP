29c8899047cfb955009bda0a43a46f850fd317f4
Fabio <41312326+fabioxyz@users.noreply.github.com>
2024-12-24 22:27:58 +0000

    Update main.c

--- main.c
+++ main.c
@@ -19,28 +19,52 @@ int main() {
     TCAutomovel cliente[50];
     int e;
     char nome[50];
-    int total = 0;// enquanto nao ler do arquivo quantos existem
+    int total;
 
+    total = LerClientes(cliente);
+    printf("Carregados : %d", total);
 
     while (1) {
         e = MENU();
         switch (e)
         {
+        case 0: return 0;
         case 1:
             total = InsereCliente(cliente, total);
             break;
         case 2: 
+            if (total == 0) {
+                printf("\nNao existem clientes guardados.\n\n");
+                break;
+            }
             ListarClientes(cliente, total);
             break;
         case 3:
-            printf("Digite o nome do Cliente : ");
-            gets(nome);
-            MostrarCliente(cliente, nome, total);
+            if (total == 0) {
+                printf("\nNao existem clientes guardados.\n\n");
+                break;
+            }
+            else {
+                printf("Digite o nome do Cliente : ");
+                gets(nome);
+                MostrarCliente(cliente, nome, total);
+            }
+            break;
+            // falta o case 4 procurar por tipo
+        case 5:
+            if (total == 0) {
+                printf("\nNao existem clientes guardados.\n\n");
+                break;
+            }
+            else {
+                GravarCliente(cliente, total);
+            }
             break;
         default:
             break;
         }
     }
+
 
 	return 0;
 }
