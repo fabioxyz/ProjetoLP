dd7ceb37b993d92975d629986887a3485d75684a
Fabio <41312326+fabioxyz@users.noreply.github.com>
2024-12-25 06:08:43 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,19 +1,5 @@
 #include <stdio.h>
-
-typedef enum {
-    Regular = 1,
-    Esporadico,
-    Ocasional
-}Tipo_de_cliente;
-
-typedef struct {
-    int numero_de_cliente;
-    char nome[50];
-    char NIF[10];//nif tem 9 numeros e o \0 no final
-    float valor_total_compras;
-    Tipo_de_cliente tipo;
-    char data_de_registo[50];
-}TCAutomovel;
+#include "head.h"
 
 int main() {
     TCAutomovel cliente[50];
@@ -22,7 +8,9 @@ int main() {
     int total;
 
     total = LerClientes(cliente);
-    printf("Carregados : %d", total);
+    if (total > 0) {
+        printf("\n %d Clientes carregados com sucesso.\n\n", total);
+    }
 
     while (1) {
         e = MENU();
